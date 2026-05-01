e4fe8e15e8d734c5b1d7a21b012ee87b8da4728b
SierraHotelRomeo <sierrahotelromeo@gmail.com>
2026-05-01 12:48:03 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+API_ID = int(os.environ["API_ID"])
+API_HASH = os.environ["API_HASH"]
+SESSION_STRING = os.environ.get("SESSION_STRING", "")
+
+client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
+
+async def main():
+    print("Telegram client started")
+
+    async for dialog in client.iter_dialogs():
+        print(dialog.name)
+
+with client:
+    client.loop.run_until_complete(main())
